c1d29391ab4bdc194e14b9f89e99c86d3ac2e48b
srsohan485 <sayedurrahmansohan@gmail.com>
2025-11-26 21:45:04 +0600

    Data Loaded In ListTile v2

--- lib/product_list_screen.dart
+++ lib/product_list_screen.dart
@@ -1,3 +1,5 @@
+import 'package:apidio/api_service.dart';
+import 'package:apidio/productmodel.dart';
 import 'package:flutter/material.dart';
 class ProductListScreen extends StatefulWidget {
   const ProductListScreen({super.key});
@@ -7,8 +9,48 @@ class ProductListScreen extends StatefulWidget {
 }
 
 class _ProductListScreenState extends State<ProductListScreen> {
+
+  ApiService apiService=ApiService();
+
+  @override
+  void initState() {  //data asar sathe sathe coll hoy
+    super.initState();
+    apiService.getAllProducts();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return FutureBuilder(
+      future: apiService.getAllProducts(),
+      builder: (context, asyncSnapshot) {
+
+        if(asyncSnapshot.connectionState == ConnectionState.waiting){  //Loding Check
+          return Center(child: CircularProgressIndicator());
+        }
+
+        if(asyncSnapshot.hasError){  //Error Check
+          return Center(child: Text("Error Fetching Products: ${asyncSnapshot.error}"),);
+        }
+
+        if(!asyncSnapshot.hasData || (asyncSnapshot.data?.products?.isEmpty ?? true)){  //Data Ace Kina Check
+          return Center(child: Text("No Product Available"));
+        }
+
+        return Scaffold(
+          body: ListView.builder(
+            itemCount: asyncSnapshot.data?.products?.length,
+              itemBuilder: (context,index){
+              Product product=asyncSnapshot.data!.products![index];
+                return ListTile(
+                  leading: Image.network(product.thumbnail ?? ""),
+                  title: Text(product.title ?? ""),
+                  subtitle: Text(product.price.toString()),
+                  onTap: (){},
+                );
+              }
+          ),
+        );
+      }
+    );
   }
 }
